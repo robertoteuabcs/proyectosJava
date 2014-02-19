c6906f69bff469ec429ae978616ae08673400b86
robertoteuabcs <robertoteapp@hotmail.com>
2014-02-19 17:38:33 -0600

    Jframe Sencillo
    Sencillo Jframe

---
+++ VentanaFrame/src/Jf.java
@@ -0,0 +1,15 @@
+import javax.swing.JFrame;
+
+
+public class Jf {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		JFrame jf =new JFrame();
+		jf.setSize(400, 400);
+		jf.setVisible(true);
+		//Hecho en Mexico
+	}
+
+}
+
