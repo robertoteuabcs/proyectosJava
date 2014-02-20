bbee954bb26fb343454895f89af5c880f5538d17
robertoteuabcs <robertoteapp@hotmail.com>
2014-02-19 18:49:07 -0600

    Ventan Frame
    Caracteristica JFrame

---
+++ VentanaFrame/src/Jentana.java
@@ -0,0 +1,32 @@
+import javax.swing.JButton;
+import javax.swing.JFrame;
+
+
+public class Jentana extends JFrame{
+	
+
+		private int altura=0;
+		private int ancho=0;
+		private String titulo="";
+		JButton btn;
+		
+		public Jentana(int ancho, int altura, String titulo){
+			this.setSize(ancho, altura);
+			this.titulo=titulo;
+			this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+			btn = new JButton("Hola mundo");
+			this.add(btn);
+			this.setVisible(true);
+		}
+		
+		public Jentana(){
+			btn = new JButton("Hola mundo");
+			this.add(btn);
+			this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+			this.setVisible(true);
+		}
+		
+		
+
+
+}
--- VentanaFrame/src/Jf.java
+++ VentanaFrame/src/Jf.java
@@ -1,14 +1,13 @@
-import javax.swing.JFrame;
+
 
 
 public class Jf {
-
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		JFrame jf =new JFrame();
-		jf.setSize(400, 400);
-		jf.setVisible(true);
-		//Hecho en Mexico
+		Jentana ventana=new Jentana();
+		ventana.setSize(300, 200);
+		
 	}
 
 }
